399174ce3643eb3d9f45acfb13601b3b8ee9750c
Anna <109809860+annkrv80@users.noreply.github.com>
2023-12-21 10:49:02 +0700

    Update settings.py

--- MagicStones/settings.py
+++ MagicStones/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n!s@7za6%l^^nevvxqqqmour7uegan^xu*4*ygavtzy0s34fo&'
+SECRET_KEY = 'django-insecure-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
